7a1735738d66f3224aa055d664b38d6b1be407db
Martins Spilners <martins.spilners@me.com>
2012-09-23 15:26:07 +0200

    Don't reset to teaser image after resizing browser window and bump to version 1.5.2

--- dribbble.safariextension/d.js
+++ dribbble.safariextension/d.js
@@ -1,6 +1,16 @@
 var images = document.getElementById('main').getElementsByTagName('img');
-for (var i=0; i<images.length; i++) {
-	var sources = images[i].getAttribute('src').replace('_teaser','');
-	images[i].setAttribute('src',sources);
-	sources = ''
+for (var i = 0; i < images.length; i++) {
+	var sources = images[i].getAttribute('src').replace('_teaser', '');
+	images[i].setAttribute('src', sources);
+}
+
+var tags = document.getElementsByTagName('div');
+for (var i = 0, tagsLength = tags.length; i < tagsLength; i++) {
+	if (tags[i].getAttribute('data-picture') !== null) {
+		var sourceTags = tags[i].getElementsByTagName('div');
+		for (var j = 0, sourceTagsLength = sourceTags.length; j < sourceTagsLength; j++) {
+			var dataSource = sourceTags[j].getAttribute('data-src').replace('_teaser', '');
+			sourceTags[j].setAttribute('data-src', dataSource);
+		}
+	}
 }
